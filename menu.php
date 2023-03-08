bc51546ffbef3fd5fb3634d1206e7a82a3b50529
Fr3gR <kazi.fregr@gmail.com>
2023-03-08 22:13:26 +0100

    Update #1

--- menu.php
+++ menu.php
@@ -1,5 +1,8 @@
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
   <div class="container-fluid">
+        <a class="navbar-brand" href="">
+        <img src="beach.png" alt="Avatar Logo" style="width:35px;"> 
+        </a>
     <a class="navbar-brand" href="index.php"><b>Hotel California</b></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
       <span class="navbar-toggler-icon"></span>
--- pobyt.php
+++ pobyt.php
@@ -4,17 +4,18 @@
   <title>HOTEL CALIFORNIA</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image" href="beach.png">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
 
-<?php require('menu.php');?>
+  <?php require('menu.php');?>
 
-<div class="container-fluid mt-3">
-  <h3>Pobyty</h3>
-  <p>Pobyty</p>
-</div>
+  <div class="container-fluid mt-3">
+    <h3>Pobyty</h3>
+    <p>Pobyty</p>
+  </div>
 
 </body>
 </html>
